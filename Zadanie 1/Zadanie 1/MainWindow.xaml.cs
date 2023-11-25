c5ca8a7c3cac77a97564e260e39de4bd1d32d4b4
SpeedWagon12 <151126812+SpeedWagon12@users.noreply.github.com>
2023-11-25 17:34:54 +0300

    Add files via upload

---
+++ Zadanie 1/Zadanie 1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(Box1.Text, out double a))
+            { MessageBox.Show("Поле только для чисел"); }
+            switch (a)
+            {
+                case 1:
+                    Answer.Text += "понедельник";
+                    break;
+                case 2:
+                    Answer.Text += "вторник";
+                    break;
+                case 3:
+                    Answer.Text += "среда";
+                    break;
+                case 4:
+                    Answer.Text += "четверг";
+                    break;
+                case 5:
+                    Answer.Text += "пятница";
+                    break;
+                case 6:
+                    Answer.Text += "суббота";
+                    break;
+                case 7:
+                    Answer.Text += "воскресенье";
+                    break;
+                default:
+                    Answer.Text +=  "Введено неверное число";
+                    break;
+            }
+
+           
+        }
+    }
+}
---
+++ Zadanie 3/Zadanie 3/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(Box1.Text, out double s))
+            { MessageBox.Show("Поле только для чисел"); }
+            if (!double.TryParse(Box2.Text, out double z))
+            { MessageBox.Show("Поле только для чисел"); }
+            double R = 0;
+            double D = 0;
+            double L = 0;
+            double S = 0;
+            switch (s)
+            {
+                case 1:
+                    Answer.Text += R = z;
+                    D = 2 * R;
+                    L = 2 * 3.14 * R;
+                    S = 3.14 * R * R;
+                    break;
+                case 2:
+                    D = z; R = D / 2; L = 2 * 3.14 * R; S = 3.14 * R * R;
+                    break;
+                case 3:
+                    L = z; R = L / (2 * 3.14); D = 2 * R; S = 3.14 * R * R;
+                    break;
+                case 4:
+                    S = z; R = Math.Sqrt(S / 3.14); D = 2 * R; L = 2 * 3.14 * R;
+                    break;
+            }
+            Answer.Text += $" D = {D:f2} R = {R:f2} L = {L:f2} S = {S:f2}";
+        }
+    }
+}
